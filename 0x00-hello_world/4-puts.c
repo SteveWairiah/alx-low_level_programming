620914a20c24ae8697c89ffc92ad327afa486251
Stephen <stevewairiah99@gmail.com>
2023-06-15 05:45:22 -0700

    "Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - puts function
+ * Return 0 (Success)
+ */
+int man(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
