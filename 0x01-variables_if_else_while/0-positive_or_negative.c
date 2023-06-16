8e6b61e55ba5f7a7118bf6e98bdab1986efafa73
Stephen <stevewairiah99@gmail.com>
2023-06-16 11:03:46 -0700

    All programs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Description: This program will assign a random number to the variable
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	ld = n % 10;
-	if (ld > 5)
+	if (n > 0)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n ld);
+		printf("%d is positive\n", n);
 	}
-	else if (ld == 0)
+	else if (n == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, ld);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n ld);
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include  <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase except e and q
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122);
+	{
+		if (n != 101 &&  n != 113)
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
