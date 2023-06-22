47afe2ab625329f64038fefcd997ead6b5d1d86e
Stephen <stevewairiah99@gmail.com>
2023-06-22 10:23:51 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,7 +4,7 @@
 
 /**
  * main -  prints the largest prime factor of the number 612852475143
- * Description:  prints the largest prime factor of the number 612852475143
+ * Description: prints the largest prime factor of the number 612852475143
  * return: Always 0 (Success)
  */
 
