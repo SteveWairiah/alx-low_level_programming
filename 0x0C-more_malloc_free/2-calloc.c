57ce5cae20d65030781f4af158917ba04d485cfe
Stephen <stevewairiah99@gmail.com>
2023-07-13 07:08:23 -0700

    New strings

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_calloc -  allocates memory for an array
+ * @nmemb: input
+ * @size: integer size
+ * Return: NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int a, b;
+	char *c;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	b = nmemb * size;
+	c = malloc(b);
+
+	if (c == NULL)
+		return (NULL);
+
+	while (a < b)
+	{
+		c[a] = 0;
+		a++;
+	}
+	return (c);
+}
