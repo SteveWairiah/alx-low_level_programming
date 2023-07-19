5a2e421f0c77adfef29a626c00c134fdfca22a91
Stephen <stevewairiah99@gmail.com>
2023-07-19 05:51:07 -0700

    Task 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,17 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - prints a name
  * @name: name
  * @f: pointer
+ * Return: Nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
