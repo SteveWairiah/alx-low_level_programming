3f55b445a16da6b920e37eeded2ee72eb246196d
SteveWairiah <stevewairiah99@gmail.com>
2023-08-03 15:10:20 +0300

    0, 1, 2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number
+ * Return: Nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	int a, count = 0;
+	unsigned long int current;
+
+	for (a = 63; a >= 0; a--)
+	{
+		current = n >> a;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
