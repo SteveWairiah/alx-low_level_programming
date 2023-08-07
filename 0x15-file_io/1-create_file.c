06803d70d0225f15a0738ce98b42cf13d09be1f9
SteveWairiah <stevewairiah99@gmail.com>
2023-08-07 13:30:21 +0300

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * create_file - creates a file
@@ -13,6 +12,9 @@ int create_file(const char *filename, char *text_content)
 	int a, b, len = 0;
 
 	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
